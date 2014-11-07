c85cb9c7c68f133d0802767fa27b0cb6b1eff1c6
jmartin <jmartin@zikzakmedia.com>
2014-11-07 14:32:34 +0100

    Cleanup tests

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
-#This file is part product_sequence module for Tryton.
-#The COPYRIGHT file at the top level of this repository contains
-#the full copyright notices and license terms.
+# This file is part product_sequence module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
 
 def register():
     pass
