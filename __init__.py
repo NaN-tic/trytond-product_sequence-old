37d28cda8e696c09103d1424b3c43518dc3f474b
Raimon Esteve <raimon@nan-tic.com>
2018-09-15 18:11:38 +0200

    Migrate to py3

--- __init__.py
+++ __init__.py
@@ -1,7 +1,3 @@
 # This file is part product_sequence module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
-
-def register():
-    pass
-
